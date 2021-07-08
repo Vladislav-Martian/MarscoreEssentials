c8b845574bf9b393c99e61549427f6d79e0fe28f
Vladislav-Martian <me.as.martian@gmail.com>
2021-07-08 15:54:01 +0300

    Short... descriptions

--- MarscoreEssentials/Memory/TemporalFile.cs
+++ MarscoreEssentials/Memory/TemporalFile.cs
@@ -4,29 +4,58 @@ using System.IO;
 
 namespace MarscoreEssentials.Memory
 {
+    /// <summary>
+    /// Represents temporal file. Can be used in using statement. On first file open saves FileStream
+    /// to poperty Stream. Stream can be closed directly on tf object by Close(), or will be closed
+    /// on Dispose() after using, but you need to use Open() to create Stream.;
+    /// </summary>
     public sealed class TemporalFile: IDisposable
     {
         #region Props
+        /// <summary>
+        /// Only file name and extension, not path!!!
+        /// </summary>
         public string Name { get; }
+        /// <summary>
+        /// Full path to file
+        /// </summary>
         public string Path { get; }
+        /// <summary>
+        /// Path to folder, where file is located
+        /// </summary>
         public string FolderPath
         {
             get => System.IO.Path.GetDirectoryName(Path);
         }
-
+        /// <summary>
+        /// Null if tf instance never opened. After Open() contains FileStream instance.
+        /// Can be closed directly on tf instance.
+        /// </summary>
         public FileStream Stream { get; private set; } = null;
-
+        /// <summary>
+        /// Used only inside, if temporal file placed inside temporal folder from class TemporalFolder.
+        /// Needed to safely remove file frome folder and dictionary inside TemporalFolder instance.
+        /// </summary>
         private TemporalFolder TemporalFolderHook { get; }
         #endregion
         
         #region Static
+        /// <summary>
+        /// Contains all ever created temporal files
+        /// </summary>
         private static List<TemporalFile> All { get; }
         static TemporalFile()
         {
             All = new List<TemporalFile>();
         }
+        /// <summary>
+        /// Extension that has every default temp file. Default: .tmp
+        /// </summary>
         public static string DefaultTemporalFileExtension { get; set; } = ".tmp";
 
+        /// <summary>
+        /// Clears all created instances and removes files.
+        /// </summary>
         public static void Clear()
         {
             var tmp = All.ToArray();
@@ -39,6 +68,12 @@ namespace MarscoreEssentials.Memory
         #endregion
 
         #region Structural
+        /// <summary>
+        /// Default constructor. Use named argument passing if need to change only second or third argument.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="dPath"></param>
+        /// <param name="hook"></param>
         public TemporalFile(in string name = null, in string dPath = null, in TemporalFolder hook = null)
         {
             Name = name ?? Guid.NewGuid().ToString() + DefaultTemporalFileExtension; 
@@ -52,7 +87,12 @@ namespace MarscoreEssentials.Memory
             All.Add(this);
             TemporalFolderHook = hook;
         }
-        
+        /// <summary>
+        /// Closes FileStream in Steam property
+        /// Removes object feom All collection
+        /// Removes file itself
+        /// Removes object from linked TemporalFolder hook.
+        /// </summary>
         public void Dispose()
         {
             Close();
@@ -67,6 +107,15 @@ namespace MarscoreEssentials.Memory
         #endregion
 
         #region Other
+        /// <summary>
+        /// Open file and put stream to property Stream
+        /// </summary>
+        /// <param name="mode"></param>
+        /// <param name="access"></param>
+        /// <param name="share"></param>
+        /// <param name="bufferSize"></param>
+        /// <param name="useAsync"></param>
+        /// <returns></returns>
         public FileStream Open(
             System.IO.FileMode mode = FileMode.OpenOrCreate, 
             System.IO.FileAccess access = FileAccess.ReadWrite, 
@@ -77,9 +126,12 @@ namespace MarscoreEssentials.Memory
             return Stream ??= new FileStream(Path, mode, access, share, bufferSize, useAsync);
         }
 
+        /// <summary>
+        /// Closes Stream
+        /// </summary>
         public void Close()
         {
-            Stream.Close();
+            Stream?.Close();
         }
         #endregion
 
--- MarscoreEssentials/Memory/TemporalFolder.cs
+++ MarscoreEssentials/Memory/TemporalFolder.cs
@@ -13,18 +13,29 @@ namespace MarscoreEssentials.Memory
         /// Only folder name, not full path
         /// </summary>
         public string Name { get; }
+        /// <summary>
+        /// Full folder path
+        /// </summary>
         public string Path { get; }
+        /// <summary>
+        /// Files inside
+        /// </summary>
         internal Dictionary<string, TemporalFile> _files{ get; }
         #endregion
 
         #region Static
+        /// <summary>
+        /// All ever created tfo
+        /// </summary>
         private static List<TemporalFolder> All { get; }
-
+        
         static TemporalFolder()
         {
             All = new List<TemporalFolder>();
         }
-
+        /// <summary>
+        /// Removes all temporal folders
+        /// </summary>
         public static void Clear()
         {
             var tmp = All.ToArray();
@@ -38,6 +49,11 @@ namespace MarscoreEssentials.Memory
         #endregion
 
         #region Structural
+        /// <summary>
+        /// Default constructor, use named argument passing
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="dPath"></param>
         public TemporalFolder(in string name = null, in string dPath = null)
         {
             Path = System.IO.Path.Combine(
@@ -50,6 +66,12 @@ namespace MarscoreEssentials.Memory
             }
             _files = new Dictionary<string, TemporalFile>();
         }
+        /// <summary>
+        /// Remove from All,
+        /// Dispose all tmp files inside (and close it streams)
+        /// Delete files
+        /// Delete folder
+        /// </summary>
         public void Dispose()
         {
             All.Remove(this);
@@ -66,21 +88,32 @@ namespace MarscoreEssentials.Memory
         #endregion
 
         #region Other
-
+        /// <summary>
+        /// Create file inside tfo
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         public TemporalFile CreateTemporalFile(in string name = null)
         {
             var tmp = new TemporalFile(name, Path, this);
             _files.Add(tmp.Name, tmp);
             return tmp;
         }
-
+        /// <summary>
+        /// Get file by [] operator, with filename and extension in one string
+        /// </summary>
+        /// <param name="key"></param>
         public TemporalFile this[string key]
         {
             get => _files[key];
         }
+        /// <summary>
+        /// Get file by index in dictionary
+        /// </summary>
+        /// <param name="index"></param>
         public TemporalFile this[int index]
         {
-            get => _files[_files.Keys.ToArray()[index]];
+            get => _files.Values.ToArray()[index];
         }
 
         #endregion
